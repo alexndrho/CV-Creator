b39634c3bdd96db88b26b2c1ffe1015a2451b0dd
Alexander Ho <ho.alexander.g@gmail.com>
2022-09-02 00:52:06 +0800

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './styles/App.css';
+import Banner from './components/Banner';
+import CVInput from './components/CVInput';
+import CVOutput from './components/CVOutput.js';
 
 function App() {
+  let [isPrintCV, setIsPrintCV] = useState(false);
+  const [personalDetails, setPersonalDetails] = useState({
+    name: '',
+    jobTitle: '',
+    address: '',
+    phone: '',
+    email: '',
+    website: '',
+    summary: ''
+  });
+  const [skillHighlights, setSkillHighlights] = useState(['', '', '']);
+  const [experiences, setExperiences] = useState([
+    { title: '', description: '' },
+    { title: '', description: '' },
+    { title: '', description: '' }
+  ]);
+  const [educations, setEducations] = useState([
+    { education: '', year: '' },
+    { education: '', year: '' },
+    { education: '', year: '' }
+  ]);
+
+  const handleSubmit = (values) => {
+    setPersonalDetails(values.personalDetails);
+    setSkillHighlights(values.skillHighlights);
+    setExperiences(values.experiences);
+    setEducations(values.educations);
+
+    setIsPrintCV(true);
+  };
+
+  const addAttribute = (attribute) => {
+    switch (attribute) {
+      case 'skillHighlights':
+        setSkillHighlights([...skillHighlights, '']);
+        break;
+      case 'experiences':
+        setExperiences([...experiences, { title: '', value: '' }]);
+        break;
+      case 'educations':
+        setEducations([...educations, { educ: '', year: '' }]);
+        break;
+      default:
+        console.warn('addAttribute wrong attribute:', attribute);
+    }
+  };
+
+  const deleteAttribute = (attribute) => {
+    switch (attribute) {
+      case 'skillHighlights':
+        setSkillHighlights(
+          skillHighlights.filter((val, index) => index !== skillHighlights.length - 1)
+        );
+        break;
+      case 'experiences':
+        setExperiences(experiences.filter((val, index) => index !== experiences.length - 1));
+        break;
+      case 'educations':
+        setEducations(educations.filter((val, index) => index !== educations.length - 1));
+        break;
+      default:
+        console.warn('deleteAttribute wrong attribute:', attribute);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Banner />
+      <main className="container d-flex">
+        <CVInput
+          personalDetails={personalDetails}
+          skillHighlights={skillHighlights}
+          experiences={experiences}
+          educations={educations}
+          onSubmit={handleSubmit}
+          addAttribute={addAttribute}
+          deleteAttribute={deleteAttribute}
+        />
+        {isPrintCV ? (
+          <CVOutput
+            personalDetails={personalDetails}
+            skillHighlights={skillHighlights}
+            experiences={experiences}
+            educations={educations}
+          />
+        ) : (
+          <></>
+        )}
+      </main>
+    </>
   );
 }
 
---
+++ src/components/Banner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Banner.css';
+
+function Banner() {
+  return (
+    <header className="banner bg-secondary p-3 d-flex justify-content-center align-items-center">
+      <h1 className="fw-semibold fs-1 text-white">CV CREATOR</h1>
+    </header>
+  );
+}
+
+export default Banner;
